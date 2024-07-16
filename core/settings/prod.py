db618d9932b8606e15ef5f99ddcbb583cd8a17f1
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2024-07-16 13:18:14 +0300

    update production settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -7,15 +7,23 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
-DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': os.getenv('DB_NAME'),
-      'USER': os.getenv('DB_USER'),
-      'PASSWORD': getpass('DB password: '),
-      'HOST': os.getenv('DB_HOST'),
-      'PORT': os.getenv('DB_PORT')
+if os.getenv('DB') == 'sqlite':
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': BASE_DIR / 'app.db',
+      }
   }
-}
+else:
+  DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': getpass('DB password: '),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+  }
 
 STATIC_ROOT = BASE_DIR / 'static'
